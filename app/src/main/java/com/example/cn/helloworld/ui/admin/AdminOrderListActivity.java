8023657c0b0edd49b48b72d56e4269164781add3
Extraordinary-FEI <3426417265@qq.com>
2025-11-26 14:53:52 +0800

    update local

--- app/src/main/java/com/example/cn/helloworld/ui/admin/AdminOrderListActivity.java
+++ app/src/main/java/com/example/cn/helloworld/ui/admin/AdminOrderListActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -28,14 +27,11 @@ import com.example.cn.helloworld.data.model.Order;
 import com.example.cn.helloworld.data.repository.AdminOrderRepository;
 import com.example.cn.helloworld.data.session.SessionManager;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
-public class AdminOrderListActivity extends AppCompatActivity implements OrderAdapter.Callback {
+public class AdminOrderListActivity extends AppCompatActivity implements AdminOrderListActivity.OrderAdapter.Callback {
 
     public static final String EXTRA_ORDERS = "extra_orders";
     private static final String[] DEFAULT_STATUSES = new String[]{
@@ -83,7 +79,7 @@ public class AdminOrderListActivity extends AppCompatActivity implements OrderAd
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerOrders);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new OrderAdapter(this);
+        adapter = new OrderAdapter((OrderAdapter.Callback) this);
         recyclerView.setAdapter(adapter);
 
         emptyView = (TextView) findViewById(R.id.textEmptyOrders);
@@ -207,92 +203,4 @@ public class AdminOrderListActivity extends AppCompatActivity implements OrderAd
                 .show();
     }
 
-    static class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
-
-        interface Callback {
-            void onEdit(Order order);
-
-            void onDelete(Order order);
-        }
-
-        private final List<Order> orders = new ArrayList<>();
-        private final SimpleDateFormat formatter = new SimpleDateFormat("MM-dd HH:mm", Locale.getDefault());
-        private final Callback callback;
-
-        OrderAdapter(Callback callback) {
-            this.callback = callback;
-        }
-
-        void submit(List<Order> list) {
-            orders.clear();
-            if (list != null) {
-                orders.addAll(list);
-            }
-            notifyDataSetChanged();
-        }
-
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_admin_order, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            Order order = orders.get(position);
-            holder.bind(order, formatter, callback);
-        }
-
-        @Override
-        public int getItemCount() {
-            return orders.size();
-        }
-
-        static class ViewHolder extends RecyclerView.ViewHolder {
-
-            private final TextView orderIdView;
-            private final TextView statusView;
-            private final TextView amountView;
-            private final TextView timeView;
-            private final Button editButton;
-            private final Button deleteButton;
-
-            ViewHolder(View itemView) {
-                super(itemView);
-                orderIdView = (TextView) itemView.findViewById(R.id.textOrderId);
-                statusView = (TextView) itemView.findViewById(R.id.textOrderStatus);
-                amountView = (TextView) itemView.findViewById(R.id.textOrderAmount);
-                timeView = (TextView) itemView.findViewById(R.id.textOrderTime);
-                editButton = (Button) itemView.findViewById(R.id.buttonEditOrder);
-                deleteButton = (Button) itemView.findViewById(R.id.buttonDeleteOrder);
-            }
-
-            void bind(final Order order, SimpleDateFormat formatter, final Callback callback) {
-                Context context = itemView.getContext();
-                orderIdView.setText(context.getString(R.string.order_id_template, order.getOrderId()));
-                statusView.setText(context.getString(R.string.order_status_template, order.getStatus()));
-                amountView.setText(context.getString(R.string.order_total_template,
-                        String.format(Locale.getDefault(), "%.2f", order.getTotalAmount())));
-                Date date = new Date(order.getCreatedAt());
-                timeView.setText(context.getString(R.string.order_created_template, formatter.format(date)));
-
-                if (callback != null) {
-                    editButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            callback.onEdit(order);
-                        }
-                    });
-                    deleteButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            callback.onDelete(order);
-                        }
-                    });
-                }
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/cn/helloworld/ui/admin/OrderAdapter.java
@@ -0,0 +1,111 @@
+package com.example.cn.helloworld.ui.admin;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.cn.helloworld.R;
+import com.example.cn.helloworld.data.model.Order;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Adapter for displaying orders in the admin order list.
+ */
+class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
+
+    interface Callback {
+        void onEdit(Order order);
+
+        void onDelete(Order order);
+    }
+
+    private final List<Order> orders = new ArrayList<>();
+    private final SimpleDateFormat formatter = new SimpleDateFormat("MM-dd HH:mm", Locale.getDefault());
+    private final Callback callback;
+
+    OrderAdapter(Callback callback) {
+        this.callback = callback;
+    }
+
+    void submit(List<Order> list) {
+        orders.clear();
+        if (list != null) {
+            orders.addAll(list);
+        }
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_admin_order, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Order order = orders.get(position);
+        holder.bind(order, formatter, callback);
+    }
+
+    @Override
+    public int getItemCount() {
+        return orders.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView orderIdView;
+        private final TextView statusView;
+        private final TextView amountView;
+        private final TextView timeView;
+        private final Button editButton;
+        private final Button deleteButton;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            orderIdView = (TextView) itemView.findViewById(R.id.textOrderId);
+            statusView = (TextView) itemView.findViewById(R.id.textOrderStatus);
+            amountView = (TextView) itemView.findViewById(R.id.textOrderAmount);
+            timeView = (TextView) itemView.findViewById(R.id.textOrderTime);
+            editButton = (Button) itemView.findViewById(R.id.buttonEditOrder);
+            deleteButton = (Button) itemView.findViewById(R.id.buttonDeleteOrder);
+        }
+
+        void bind(final Order order, SimpleDateFormat formatter, final Callback callback) {
+            Context context = itemView.getContext();
+            orderIdView.setText(context.getString(R.string.order_id_template, order.getOrderId()));
+            statusView.setText(context.getString(R.string.order_status_template, order.getStatus()));
+            amountView.setText(context.getString(R.string.order_total_template,
+                    String.format(Locale.getDefault(), "%.2f", order.getTotalAmount())));
+            Date date = new Date(order.getCreatedAt());
+            timeView.setText(context.getString(R.string.order_created_template, formatter.format(date)));
+
+            if (callback != null) {
+                editButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        callback.onEdit(order);
+                    }
+                });
+                deleteButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        callback.onDelete(order);
+                    }
+                });
+            }
+        }
+    }
+}
